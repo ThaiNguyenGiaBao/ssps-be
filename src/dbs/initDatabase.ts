9f26e122b9ac7df71ee4bb190d1e858dcd54fc17
Gia Bao <thainguyengiabao27092004@gmail.com>
2024-10-24 22:18:41 +0700

    update db

--- src/dbs/initDatabase.ts
+++ src/dbs/initDatabase.ts
@@ -2,28 +2,19 @@ const { Pool } = require("pg");
 import dotenv from "dotenv";
 dotenv.config();
 
+let db: any;
 // Create a new pool instance
-const db = new Pool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    ssl: {
-        rejectUnauthorized: false // required for SSL connections
-    },
-    max: 20,
-    idleTimeoutMillis: 300000,
-});
-
-setInterval(async () => {
-    try {
-        await db.query('SELECT 1');
-        console.log('Connection is alive');
-    } catch (err) {
-        console.error('Error keeping connection alive:', err);
-    }
-}, 100000);
+try {
+    db = new Pool({
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
+    });
+} catch (err: any) {
+    console.log("Error in creating pool", err.message);
+}
 
 // Connect to the PostgreSQL database
 db.connect()
