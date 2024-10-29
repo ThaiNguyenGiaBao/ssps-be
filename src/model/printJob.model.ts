b5d090d320b6fced5dd67d8ff2a05b7d897887bf
Phạm Ngọc Long <77040324+pnlong2706@users.noreply.github.com>
2024-10-30 01:50:46 +0700

    Fix uuid error on query

--- src/model/printJob.model.ts
+++ src/model/printJob.model.ts
@@ -3,13 +3,13 @@ import { BadRequestError, NotFoundError } from "../helper/errorRespone";
 
 class PrintingJobModel {
     static async getPrintJob(printJobId: string) {
-        const printJob = await db.query("SELECT * FROM printingjob WHERE id = $1", [printJobId]);
+        const printJob = await db.query("SELECT * FROM printingjob WHERE id::text = '" + printJobId + "'");
         if(printJob.rows.length == 0) throw new NotFoundError("Can not find this printjob!");
         return printJob.rows[0];
     }
 
     static async getPrintJobByUser({userId, startDate, endDate}: {userId: string, startDate: string, endDate: string}) {
-        let query_string = "SELECT * FROM printingjob WHERE userid = '" + userId + "'";
+        let query_string = "SELECT * FROM printingjob WHERE userid::text = '" + userId + "'";
         if(startDate != null) query_string += " AND starttime>='"+ startDate + "'";
         if(endDate != null) query_string += " AND starttime<='"+ endDate + "'";
 
@@ -18,7 +18,7 @@ class PrintingJobModel {
     }
 
     static async getPrintJobByPrinter({printerId, startDate, endDate}: {printerId: string, startDate: string, endDate: string}) {
-        let query_string = "SELECT * FROM printingjob WHERE printerid = '" + printerId + "'";
+        let query_string = "SELECT * FROM printingjob WHERE printerid::text = '" + printerId + "'";
         if(startDate != null) query_string += " AND starttime >= '"+ startDate + "' ";
         if(endDate != null) query_string += " AND starttime <= '"+ endDate + "' ";
 
@@ -27,7 +27,7 @@ class PrintingJobModel {
     }
 
     static async getPrintJobByUserAndPrinter({userId, printerId, startDate, endDate}: {userId: string, printerId: string, startDate: string, endDate: string}) {
-        let query_string = "SELECT * FROM printingjob WHERE userid = '" + userId + "' AND printerid = '" +  printerId + "'";
+        let query_string = "SELECT * FROM printingjob WHERE userid::text = '" + userId + "' AND printerid::text = '" +  printerId + "'";
         if(startDate != null) query_string += " AND starttime >= '"+ startDate + "' ";
         if(endDate != null) query_string += " AND starttime <= '"+ endDate + "' ";
 
@@ -48,7 +48,7 @@ class PrintingJobModel {
     }
 
     static async deletePrintJob(printJobId: string) {
-        const DeleteExistPrintJob = await db.query("DELETE FROM printingjob WHERE id = $1", [printJobId]);
+        const DeleteExistPrintJob = await db.query("DELETE FROM printingjob WHERE id::text = $1", [printJobId]);
     }
 
     static async updateStatus(printJobId: string, newStatus: string) {
