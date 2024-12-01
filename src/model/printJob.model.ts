ad72bf5004b584644f674ffb25d08c9aa6f16721
Pham Ngoc Long <77040324+pnlong2706@users.noreply.github.com>
2024-12-01 22:14:24 +0700

    add more field to response in prinjob route

--- src/model/printJob.model.ts
+++ src/model/printJob.model.ts
@@ -3,7 +3,15 @@ import { BadRequestError, NotFoundError } from "../helper/errorRespone";
 
 class PrintingJobModel {
     static async getPrintJob(printJobId: string) {
-        const printJob = await db.query("SELECT * FROM printingjob WHERE id::text = '" + printJobId + "'");
+        const printJob = await db.query("SELECT printingjob.id, printingjob.printerid, printingjob.userid, printingjob.fileid, printingjob.starttime, printingjob.papersize, printingjob.numpage, \
+                                printingjob.numside, printingjob.numcopy, printingjob.pagepersheet, printingjob.colortype, printingjob.orientation, printingjob.status, users.name AS username, \
+                                users.email AS useremail, printer.brand AS printerbrand, printer.model AS printermodel, file.filename, file.url AS fileurl, file.type as filetype \
+                                FROM printingjob \
+                                INNER JOIN users ON printingjob.userid = users.id \
+                                INNER JOIN printer ON printingjob.printerid = printer.id \
+                                INNER JOIN file ON printingjob.fileid = file.id \
+                                WHERE printingjob.id::text = '" + printJobId + "'");
+
         if(printJob.rows.length == 0) throw new NotFoundError("Can not find this printjob!");
         return printJob.rows[0];
     }
@@ -11,11 +19,19 @@ class PrintingJobModel {
     static async getPrintJobByUser({userId, startDate, endDate, PageNum, itemPerPage}: 
                                    {userId: string, startDate: string, endDate: string, PageNum: number, itemPerPage: number}) {
 
-        let query_string = "SELECT * FROM printingjob WHERE userid::text = '" + userId + "' ";
-        if(startDate != null) query_string += " AND starttime>='"+ startDate + "' ";
-        if(endDate != null) query_string += " AND starttime<='"+ endDate + "' ";
+        let query_string = "SELECT printingjob.id, printingjob.printerid, printingjob.userid, printingjob.fileid, printingjob.starttime, printingjob.papersize, printingjob.numpage, \
+                                printingjob.numside, printingjob.numcopy, printingjob.pagepersheet, printingjob.colortype, printingjob.orientation, printingjob.status, users.name AS username, \
+                                users.email AS useremail, printer.brand AS printerbrand, printer.model AS printermodel, file.filename, file.url AS fileurl, file.type as filetype \
+                                FROM printingjob \
+                                INNER JOIN users ON printingjob.userid = users.id \
+                                INNER JOIN printer ON printingjob.printerid = printer.id \
+                                INNER JOIN file ON printingjob.fileid = file.id \
+                                WHERE printingjob.userid::text = '" + userId + "' ";
+
+        if(startDate != null) query_string += " AND printingjob.starttime>='"+ startDate + "' ";
+        if(endDate != null) query_string += " AND printingjob.starttime<='"+ endDate + "' ";
         
-        query_string += "ORDER BY starttime DESC ";
+        query_string += "ORDER BY printingjob.starttime DESC ";
         // if(Number.isInteger(PageNum) && PageNum > 0 && Number.isInteger(itemPerPage) && itemPerPage > 0) 
         //     query_string += "LIMIT " + itemPerPage.toString() + " OFFSET " + (itemPerPage * (PageNum - 1)).toString();
         
@@ -26,7 +42,15 @@ class PrintingJobModel {
     static async getPrintJobByPrinter({printerId, startDate, endDate, PageNum, itemPerPage}: 
                                       {printerId: string, startDate: string, endDate: string, PageNum: number, itemPerPage: number}) {
 
-        let query_string = "SELECT * FROM printingjob WHERE printerid::text = '" + printerId + "' ";
+        let query_string = "SELECT printingjob.id, printingjob.printerid, printingjob.userid, printingjob.fileid, printingjob.starttime, printingjob.papersize, printingjob.numpage, \
+                                printingjob.numside, printingjob.numcopy, printingjob.pagepersheet, printingjob.colortype, printingjob.orientation, printingjob.status, users.name AS username, \
+                                users.email AS useremail, printer.brand AS printerbrand, printer.model AS printermodel, file.filename, file.url AS fileurl, file.type as filetype \
+                                FROM printingjob \
+                                INNER JOIN users ON printingjob.userid = users.id \
+                                INNER JOIN printer ON printingjob.printerid = printer.id \
+                                INNER JOIN file ON printingjob.fileid = file.id \
+                                WHERE printingjob.printerid::text = '" + printerId + "' ";
+
         if(startDate != null) query_string += " AND starttime >= '"+ startDate + "' ";
         if(endDate != null) query_string += " AND starttime <= '"+ endDate + "' ";
 
@@ -41,7 +65,15 @@ class PrintingJobModel {
     static async getPrintJobByUserAndPrinter({userId, printerId, startDate, endDate, PageNum, itemPerPage}: 
                                              {userId: string, printerId: string, startDate: string, endDate: string, PageNum: number, itemPerPage: number}) {
 
-        let query_string = "SELECT * FROM printingjob WHERE userid::text = '" + userId + "' AND printerid::text = '" +  printerId + "' ";
+        let query_string = "SELECT printingjob.id, printingjob.printerid, printingjob.userid, printingjob.fileid, printingjob.starttime, printingjob.papersize, printingjob.numpage, \
+                                printingjob.numside, printingjob.numcopy, printingjob.pagepersheet, printingjob.colortype, printingjob.orientation, printingjob.status, users.name AS username, \
+                                users.email AS useremail, printer.brand AS printerbrand, printer.model AS printermodel, file.filename, file.url AS fileurl, file.type as filetype \
+                                FROM printingjob \
+                                INNER JOIN users ON printingjob.userid = users.id \
+                                INNER JOIN printer ON printingjob.printerid = printer.id \
+                                INNER JOIN file ON printingjob.fileid = file.id \
+                                WHERE printingjob.userid::text = '" + userId + "' AND printerid::text = '" +  printerId + "' ";
+
         if(startDate != null) query_string += " AND starttime >= '"+ startDate + "' ";
         if(endDate != null) query_string += " AND starttime <= '"+ endDate + "' ";
 
@@ -54,14 +86,21 @@ class PrintingJobModel {
     }
 
     static async getPrintJobByDuration({startDate, endDate, PageNum, itemPerPage}: {startDate: string, endDate: string, PageNum: number, itemPerPage: number}) {
-        let query_string = "SELECT * FROM printingjob";
-        if(startDate != null) {
-            query_string += " WHERE starttime >= '"+ startDate + "' ";
-            if(endDate != null) query_string += " AND starttime <= '"+ endDate + "' ";
-        }
-        else if(endDate != null) query_string += " WHERE starttime <= '"+ endDate + "' ";
+        let query_string = "SELECT printingjob.id, printingjob.printerid, printingjob.userid, printingjob.fileid, printingjob.starttime, printingjob.papersize, printingjob.numpage, \
+                                printingjob.numside, printingjob.numcopy, printingjob.pagepersheet, printingjob.colortype, printingjob.orientation, printingjob.status, users.name AS username, \
+                                users.email AS useremail, printer.brand AS printerbrand, printer.model AS printermodel, file.filename, file.url AS fileurl, file.type as filetype \
+                                FROM printingjob \
+                                INNER JOIN users ON printingjob.userid = users.id \
+                                INNER JOIN printer ON printingjob.printerid = printer.id \
+                                INNER JOIN file ON printingjob.fileid = file.id ";
 
-        query_string += "ORDER BY starttime DESC ";
+        if(startDate != null) {
+            query_string += " WHERE printingjob.starttime >= '"+ startDate + "' ";
+            if(endDate != null) query_string += " AND printingjob.starttime <= '"+ endDate + "' ";
+        }
+        else if(endDate != null) query_string += " WHERE printingjob.starttime <= '"+ endDate + "' ";
+
+        query_string += "ORDER BY printingjob.starttime DESC ";
         // if(Number.isInteger(PageNum) && PageNum > 0 && Number.isInteger(itemPerPage) && itemPerPage > 0) 
         //     query_string += "LIMIT " + itemPerPage.toString() + " OFFSET " + (itemPerPage * (PageNum - 1)).toString();
         
