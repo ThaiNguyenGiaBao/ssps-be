3f5bf66902e1f818440b9dbe33cbc261b55ba468
Gia Bao <thainguyengiabao27092004@gmail.com>
2025-03-17 12:54:02 +0700

    'build'

--- dist/app.js
+++ dist/app.js
@@ -18,9 +18,8 @@ app.use((0, compression_1.default)());
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use((0, cors_1.default)({
-    origin: ["http://localhost:3002", "https://smartstudentprintingservice.vercel.app/"], // Allow all origins
+    origin: true, // This is a security issue, allowing all origins
     credentials: true // This allows cookies to be sent/received
-    //methods: ["GET", "POST", "PUT", "PATCH","DELETE", "OPTIONS"] // Allow OPTIONS for preflight
 }));
 app.use((0, cookie_parser_1.default)());
 // init router
